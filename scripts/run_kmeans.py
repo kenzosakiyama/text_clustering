e9f56e7e138362967f61cfedd5230773822ec610
Kenzo <kenzosakiyama@gmail.com>
2022-06-10 20:21:59 -0300

    Permitindo a escolha da metrica para estimar o K

--- scripts/run_kmeans.py
+++ scripts/run_kmeans.py
@@ -15,6 +15,7 @@ if __name__ == "__main__":
     parser = ArgumentParser()
 
     parser.add_argument("--embeddings", type=str, required=True, help="Arquivo .pkl com os embeddings a serem utilizados.")
+    parser.add_argument("--metric", type=str, default="distortion", help="Métrica utilizada para escolha do K.")
     parser.add_argument("--plot_file", type=str, default="elbow.png", help="Caminho para salvar uma imagem contendo a visualização da regra do joelho.")
     parser.add_argument("--output_file", type=str, default="clusters.pkl", help="Arquivo .pkl para armazenar os clusters. Associa-se um cluster para cada exemplo do arquivo de embeddings.")
 
@@ -27,8 +28,8 @@ if __name__ == "__main__":
     print(f"- Saving clusters to: {args.output_file}.")
 
     clustering_model = KMeans(random_state=1234, n_init=200, max_iter=500)
-    print(f"- Running elbow rule.")
-    model = KElbowVisualizer(clustering_model, k=K_RANGES, metric="distortion")
+    print(f"- Running elbow rule, using {args.metric} to estimate K.")
+    model = KElbowVisualizer(clustering_model, k=K_RANGES, metric="silhouette")
     model.fit(text_vectors)
 
     model.show(args.plot_file)
